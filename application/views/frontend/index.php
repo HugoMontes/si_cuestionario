1bfaf0e54044eea9f373bd2739be9505504fecfb
Felix Montes <hugomontes927@gmail.com>
2017-08-27 14:53:07 -0400

    Se ha agregado la platilla para el backend

---
+++ application/views/frontend/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistema Cuestionario</title>
+</head>
+<body>
+	<h1>Bienvenido</h1>
+	<a href="<?php echo base_url();?>index.php/backend/escritorio">Administrador</a>
+</body>
+</html>
